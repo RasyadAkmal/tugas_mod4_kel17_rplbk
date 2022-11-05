a1ccf1297360f196d5377fda3f311d7f67a63e61
Nuzulitastwn <63113078+Nuzulitastwn@users.noreply.github.com>
2022-11-05 09:27:14 +0700

    Updated New Page

--- src/new-page/Index.js
+++ src/new-page/Index.js
@@ -8,4 +8,15 @@ export default function New() {
     count.current = count.current + 1;
   });
 
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+    </>
+  );
+}
 
